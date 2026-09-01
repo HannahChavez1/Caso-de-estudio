eb34f0b94464c0b8f9760eb70ea279f1a96a3330
Ale_RL <lorozco22@ucol.mx>
2026-09-01 13:52:45 -0600

    CASO DE ESTUDIO 1

--- Caso de estudio/Form1.Designer.cs
+++ Caso de estudio/Form1.Designer.cs
@@ -28,18 +28,250 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            txtNombre = new TextBox();
+            panel1 = new Panel();
+            chkLectura = new CheckBox();
+            chkProgramacion = new CheckBox();
+            chkArte = new CheckBox();
+            chkMusica = new CheckBox();
+            chkDeportes = new CheckBox();
+            panel2 = new Panel();
+            rbLinea = new RadioButton();
+            rbHibrida = new RadioButton();
+            rbPresencial = new RadioButton();
+            label3 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            btbLimpiar = new Button();
+            label4 = new Label();
+            txtResumen = new TextBox();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(112, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Nombre del usuario";
+            label1.Click += label1_Click;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(130, 27);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(209, 23);
+            txtNombre.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(chkLectura);
+            panel1.Controls.Add(chkProgramacion);
+            panel1.Controls.Add(chkArte);
+            panel1.Controls.Add(chkMusica);
+            panel1.Controls.Add(chkDeportes);
+            panel1.Location = new Point(12, 120);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(241, 139);
+            panel1.TabIndex = 2;
+            // 
+            // chkLectura
+            // 
+            chkLectura.AutoSize = true;
+            chkLectura.Location = new Point(11, 81);
+            chkLectura.Name = "chkLectura";
+            chkLectura.Size = new Size(65, 19);
+            chkLectura.TabIndex = 4;
+            chkLectura.Text = "Lectura";
+            chkLectura.UseVisualStyleBackColor = true;
+            // 
+            // chkProgramacion
+            // 
+            chkProgramacion.AutoSize = true;
+            chkProgramacion.Location = new Point(11, 107);
+            chkProgramacion.Name = "chkProgramacion";
+            chkProgramacion.Size = new Size(101, 19);
+            chkProgramacion.TabIndex = 3;
+            chkProgramacion.Text = "Programacion";
+            chkProgramacion.UseVisualStyleBackColor = true;
+            // 
+            // chkArte
+            // 
+            chkArte.AutoSize = true;
+            chkArte.Location = new Point(11, 56);
+            chkArte.Name = "chkArte";
+            chkArte.Size = new Size(48, 19);
+            chkArte.TabIndex = 2;
+            chkArte.Text = "Arte";
+            chkArte.UseVisualStyleBackColor = true;
+            // 
+            // chkMusica
+            // 
+            chkMusica.AutoSize = true;
+            chkMusica.Location = new Point(11, 31);
+            chkMusica.Name = "chkMusica";
+            chkMusica.Size = new Size(64, 19);
+            chkMusica.TabIndex = 1;
+            chkMusica.Text = "Musica";
+            chkMusica.UseVisualStyleBackColor = true;
+            chkMusica.CheckedChanged += checkBox2_CheckedChanged;
+            // 
+            // chkDeportes
+            // 
+            chkDeportes.AutoSize = true;
+            chkDeportes.Location = new Point(11, 6);
+            chkDeportes.Name = "chkDeportes";
+            chkDeportes.Size = new Size(73, 19);
+            chkDeportes.TabIndex = 0;
+            chkDeportes.Text = "Deportes";
+            chkDeportes.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(rbLinea);
+            panel2.Controls.Add(rbHibrida);
+            panel2.Controls.Add(rbPresencial);
+            panel2.Location = new Point(330, 120);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(209, 139);
+            panel2.TabIndex = 3;
+            // 
+            // rbLinea
+            // 
+            rbLinea.AutoSize = true;
+            rbLinea.Location = new Point(20, 81);
+            rbLinea.Name = "rbLinea";
+            rbLinea.Size = new Size(66, 19);
+            rbLinea.TabIndex = 2;
+            rbLinea.TabStop = true;
+            rbLinea.Text = "En linea";
+            rbLinea.UseVisualStyleBackColor = true;
+            // 
+            // rbHibrida
+            // 
+            rbHibrida.AutoSize = true;
+            rbHibrida.Location = new Point(20, 31);
+            rbHibrida.Name = "rbHibrida";
+            rbHibrida.Size = new Size(64, 19);
+            rbHibrida.TabIndex = 1;
+            rbHibrida.TabStop = true;
+            rbHibrida.Text = "Hibrida";
+            rbHibrida.UseVisualStyleBackColor = true;
+            // 
+            // rbPresencial
+            // 
+            rbPresencial.AutoSize = true;
+            rbPresencial.Location = new Point(20, 56);
+            rbPresencial.Name = "rbPresencial";
+            rbPresencial.Size = new Size(78, 19);
+            rbPresencial.TabIndex = 0;
+            rbPresencial.TabStop = true;
+            rbPresencial.Text = "Presencial";
+            rbPresencial.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(330, 102);
+            label3.Name = "label3";
+            label3.Size = new Size(114, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Modalidad Preferida";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 102);
+            label2.Name = "label2";
+            label2.Size = new Size(217, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Actividades favoritas (ocpvion multiple)";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(315, 311);
+            button1.Name = "button1";
+            button1.Size = new Size(114, 23);
+            button1.TabIndex = 6;
+            button1.Text = "Mostrar resumen";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // btbLimpiar
+            // 
+            btbLimpiar.Location = new Point(464, 311);
+            btbLimpiar.Name = "btbLimpiar";
+            btbLimpiar.Size = new Size(75, 23);
+            btbLimpiar.TabIndex = 7;
+            btbLimpiar.Text = "Limpiar";
+            btbLimpiar.UseVisualStyleBackColor = true;
+            btbLimpiar.Click += btbLimpiar_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(26, 355);
+            label4.Name = "label4";
+            label4.Size = new Size(62, 15);
+            label4.TabIndex = 9;
+            label4.Text = "Resumen :";
+            // 
+            // txtResumen
+            // 
+            txtResumen.Location = new Point(12, 373);
+            txtResumen.Multiline = true;
+            txtResumen.Name = "txtResumen";
+            txtResumen.Size = new Size(569, 183);
+            txtResumen.TabIndex = 10;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(512, 547);
+            ClientSize = new Size(593, 568);
+            Controls.Add(txtResumen);
+            Controls.Add(label4);
+            Controls.Add(btbLimpiar);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(txtNombre);
+            Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox txtNombre;
+        private Panel panel1;
+        private Panel panel2;
+        private Label label3;
+        private CheckBox chkLectura;
+        private CheckBox chkProgramacion;
+        private CheckBox chkArte;
+        private CheckBox chkMusica;
+        private CheckBox chkDeportes;
+        private RadioButton rbLinea;
+        private RadioButton rbHibrida;
+        private RadioButton rbPresencial;
+        private Label label2;
+        private Button button1;
+        private Button btbLimpiar;
+        private Label label4;
+        private TextBox txtResumen;
     }
 }
--- Caso de estudio/Form1.cs
+++ Caso de estudio/Form1.cs
@@ -6,5 +6,149 @@ namespace Caso_de_estudio
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //====================================
+            // 1.- VALIDAR EL NOMBRE
+            //====================================
+            if (string.IsNullOrWhiteSpace(txtNombre.Text))
+            {
+                MessageBox.Show(
+                  "Por favor escribe tu nombre.",
+                   "Validación",
+                      MessageBoxButtons.OK,
+                          MessageBoxIcon.Warning
+                        );
+                txtNombre.Focus();
+            }
+            //=========================================
+            // 2.- OBTENER LAS ACTIIDADES SELECCIONADAS
+            //=========================================
+
+            List<string> Actividades = new List<string>();
+            if (chkDeportes.Checked)
+            {
+                Actividades.Add("Deportes");
+            }
+            if (chkMusica.Checked)
+            {
+                Actividades.Add("Musica");
+            }
+            if (chkArte.Checked)
+            {
+                Actividades.Add("Arte");
+            }
+            if (chkProgramacion.Checked)
+            {
+                Actividades.Add("Programacion");
+            }
+            if (chkLectura.Checked)
+            {
+                Actividades.Add("Lectura");
+            }
+
+            //======================================
+            // 3.- COMPROBAR SELECCIÓN
+            //======================================
+            if (Actividades.Count == 0)
+            {
+                MessageBox.Show(
+                    "por favor seleciona una atividad minimo.",
+                    "validacion.",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                    );
+            }
+            //======================================
+            // 4.- OBTENER LA MODALIDAD
+            //======================================
+            string modaliad = "";
+            if (rbPresencial.Checked)
+            {
+                modaliad = "Precencial";
+            }
+            else if (rbHibrida.Checked)
+            {
+                modaliad = "Hibrida";
+            }
+            else if (rbLinea.Checked)
+            {
+                modaliad = "En linea";
+            }
+            else
+            {
+                MessageBox.Show("Porfavor seleccione una modalidad",
+                    "validacion", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            //======================================
+            // 4.- CREAR EL RESUMEN
+            //======================================
+            string resumen =
+                "Resumen de preferencia\r\n" +
+                "-------------------------\r\n" +
+                "Nombre: " + txtNombre.Text + "\r\n" +
+                "Actividades: " + string.Join(",", Actividades) + "\r\n" +
+                "Modalidad: " + modaliad;
+            txtResumen.Text = resumen;
+
+
+        }
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            //limpiar nombre
+            txtNombre.Clear(); //txtNombre="";
+            //desmarcamos el checkBox
+            chkDeportes.Checked = false;
+            chkLectura.Checked = false;
+            chkArte.Checked = false;
+            chkMusica.Checked = false;
+            chkProgramacion.Checked = false;
+            //desmarcar RadioButton
+            rbLinea.Checked = false;
+            rbHibrida.Checked = false;
+            rbPresencial.Checked = false;
+            //Limpiar Resumen
+            txtResumen.Clear();
+            //Regesamos el cursor al nombre
+            txtNombre.Focus();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btbLimpiar_Click(object sender, EventArgs e)
+        {
+            //limpiar nombre
+            txtNombre.Clear(); //txtNombre="";
+            //desmarcamos el checkBox
+            chkDeportes.Checked = false;
+            chkLectura.Checked = false;
+            chkArte.Checked = false;
+            chkMusica.Checked = false;
+            chkProgramacion.Checked = false;
+            //desmarcar RadioButton
+            rbLinea.Checked = false;
+            rbHibrida.Checked = false;
+            rbPresencial.Checked = false;
+            //Limpiar Resumen
+            txtResumen.Clear();
+            //Regesamos el cursor al nombre
+            txtNombre.Focus();
+        }
     }
 }
+
+
